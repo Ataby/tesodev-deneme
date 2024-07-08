e6856512fbc8f533ed32b9636dafc9a3b9ff5795
Muhammed Atabay <matabay200069@gmail.com>
2024-07-08 12:43:09 +0300

    landing page components

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,23 +1,20 @@
 import { MapIcon, SearchIcon } from "../../assets/";
 
-import styles from "./searchinput.module.scss";
+import styles from "./SearchInput.module.scss";
 
 const SearchInput = () => {
   return (
     <div className={styles.searchContainer}>
       <div className={styles["search-form"]}>
         <input
-           
-          placeholder={"Some..."}
-           
-           
+          placeholder={"Some..."}         
           className={styles["search-input"]}
         />
         <div className={styles["search-button"]}>
           <SearchIcon />
         </div>
         <div className={styles["input-result-container-one"]}>
-           
+           DUZENLENECEK
         </div>
       </div>
     </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import Button from "../components/Button/Button";
 import Header from '../components/Landing/Header';
-import Logo from "../../src/assets/logo.svg"
+import {Logo} from "../../src/assets/logo.svg";
+import SearchInput from '../components/SearchInput/SearchInput';
+import Carousel from '../components/Carousel/Carousel';
+import Footer from '../components/Landing/Footer';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <Button text={"Search"}/>
-    </div>
+      <>
+      <div className="container">
+        <main className="mainContainer">
+          <Header />
+          <div className="findInContainer">
+            <div>
+              Logo eklenecek
+              <div className=" logoDescription">Search App</div>
+            </div>
+
+            <div className="w-full mt-5 justify-center items-center d-flex flex-col">
+              <div className="searchContainer ">
+                <p className="heading">Find in records</p>
+
+                <div className="w-full d-flex flex-row justify-center items-center ">
+                  <SearchInput/>
+                  <div className="landingSearchButtonContainer">
+                    <Button text={"Search"} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="carouselSection">
+            <div className="d-flex  justify-center items-center flex-col">
+              <div className="text-start w-full ">
+                <p className="heading">Top News</p>
+              </div>
+
+              <div className="d-flex flex-col">
+                
+                <Carousel/>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
 
