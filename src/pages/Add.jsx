bbd847886549cbbdbdc33ccf07641fec616f2538
Muhammed Atabay <matabay200069@gmail.com>
2024-07-13 02:19:39 +0300

    adding record completed

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { useFormik } from "formik";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Header from "../components/AddPage/Header";
+import Form from "../components/Form/Form";
+import validationSchema from "../components/Form/validation";
+import { addUser } from "../database/useDatabase";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+  const { state } = useLocation();
 
-export default Add
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    validateOnMount: true,
+    initialValues: {
+      NameSurname: "",
+      Country: "",
+      Company: "",
+      City: "",
+      Email: "",
+      Date: new Date().getFullYear().toString(),
+    },
+
+    validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      try {
+        addUser(values);
+        toast("Added Succesfull", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        resetForm();
+      } catch (e) {
+        toast(e, {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    },
+  });
+  return (
+    <>
+      <div className="container">
+        <Header searchValue={state} />
+        <main className="mainContainer">
+          <div className="d-flex w-full  flex-col justify-center items-center">
+            <div className="w-full ">
+              <div className="resultContainer">
+                <Form formik={formik} />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
+export default Add;
