2beec8e11bfc2419c317119092b310eace0af180
Muhammed Atabay <matabay200069@gmail.com>
2024-07-13 00:42:55 +0300

    loading spinner

---
+++ src/components/Spinner/spinner.jsx
@@ -0,0 +1,15 @@
+ 
+import React from 'react'
+import styles from './spinner.module.scss'
+
+const Spinner = () => {
+  return (
+    <div className={styles.spinnerContainer}>
+      <div className={styles.spinner}></div>
+
+    </div>
+  )
+  
+}
+
+export default Spinner
