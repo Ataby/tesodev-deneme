8ccd9669104cc7afacbd64e3b67c49228b7f43b3
Muhammed Atabay <matabay200069@gmail.com>
2024-07-13 14:59:58 +0300

    styled button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,20 @@
 import styles from "./button.module.scss";
 
-
-const Button = ({ text, onClick, type, disabled=false }) => {
-  const buttonStyle = disabled 
-  ? { background: 'gray' }
-    : { background:"#204080" } ;
-
-      return (
-        <button style={buttonStyle} disabled={disabled} onClick={onClick} type={type || "button"} className={styles.button}>
-          {text}
-        </button>
-      );
-    };
+const Button = ({ text, onClick, type, disabled = false }) => {
+  const buttonStyle = disabled
+    ? { background: "gray" }
+    : { background: "#204080" };
+  return (
+    <button
+      style={buttonStyle}
+      disabled={disabled}
+      onClick={onClick}
+      type={type || "button"}
+      className={styles.button}
+    >
+      {text}
+    </button>
+  );
+};
 
 export default Button;
