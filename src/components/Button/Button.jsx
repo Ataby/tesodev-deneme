7d75d2521b75bd316a7236d27be76bf5e7cdae97
Muhammed Atabay <matabay200069@gmail.com>
2024-07-13 04:23:32 +0300

    search button changed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,13 @@
 import styles from "./button.module.scss";
 
 
-const Button = ({ text, onClick, type }) => {
+const Button = ({ text, onClick, type, disabled=false }) => {
+  const buttonStyle = disabled 
+  ? { background: 'gray' }
+    : { background:"#204080" } ;
+
       return (
-        <button onClick={onClick} type={type || "button"} className={styles.button}>
+        <button style={buttonStyle} disabled={disabled} onClick={onClick} type={type || "button"} className={styles.button}>
           {text}
         </button>
       );
