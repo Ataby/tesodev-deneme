ae78f0711ed6fd54c3ac590efc732058eb9a2648
Muhammed Atabay <matabay200069@gmail.com>
2024-07-13 15:07:14 +0300

    added website yup

--- src/components/Form/validation.jsx
+++ src/components/Form/validation.jsx
@@ -1,10 +1,11 @@
 import * as yup from "yup";
 
 const validations = yup.object().shape({
-  NameSurname: yup.string().min(4).max(60).required("Required Field"),
-  Email: yup.string().email().required("Required Field"),
-  Country: yup.string().min(2).max(40).required("Required Field"),
-  City: yup.string().min(2).max(40).required("Required Field"),
+  nameSurname: yup.string().min(4).max(60).required("Required Field"),
+  email: yup.string().email().required("Required Field"),
+  country: yup.string().min(2).max(40).required("Required Field"),
+  city: yup.string().min(2).max(40).required("Required Field"),
+  website: yup.string().required("Required Field")
 });
 
 export default validations;
